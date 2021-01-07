718b78b35e21043317e1604ab5eb54e238446109
a-karagiannis <77013069+a-karagiannis@users.noreply.github.com>
2021-01-07 20:28:47 +0100

    Add files via upload

---
+++ src/thermophysicalModels/basic/psiThermo/psiThermos.C
@@ -0,0 +1,80 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "psiThermo.H"
+#include "makeThermo.H"
+
+#include "specie.H"
+#include "perfectGas.H"
+#include "VLMEoS.H"
+
+#include "hRefConstThermo.H"
+#include "sensibleInternalEnergy.H"
+#include "thermo.H"
+#include "VLMThermo.H"
+
+#include "VLMTransport.H"
+
+#include "hePsiThermo.H"
+#include "pureMixture.H"
+
+#include "thermoPhysicsTypes.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+/* * * * * * * * * * * * * * Internal-energy-based * * * * * * * * * * * * * */
+
+makeThermos
+(
+    psiThermo,
+    hePsiThermo,
+    pureMixture,
+    VLMTransport,
+    sensibleInternalEnergy,
+    hRefConstThermo,
+    perfectGas,
+    specie
+);
+
+makeThermos
+(
+    psiThermo,
+    hePsiThermo,
+    pureMixture,
+    VLMTransport,
+    sensibleInternalEnergy,
+    VLMThermo,
+    VLMEoS,
+    specie
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
